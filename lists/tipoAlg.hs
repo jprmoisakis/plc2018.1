35c323013323b472cd860fd596b97d6bb095cc6d
Vinicius Oliveira <viniaguiar9509@gmail.com>
2018-04-17 10:17:47 -0300

    lista nova

--- lists/tipoAlg.hs
+++ lists/tipoAlg.hs
@@ -7,3 +7,18 @@ mdc a b
     | (max a b) `mod` (min a b) == 0 = min a b
     | (max a b) `mod` (min a b) /= 0 = mdc (min a b) ((max a b) `mod` (min a b))
 
+-- Saber se é primo
+
+-- distancia entre pontos
+type Coord = Double
+data Pontos = Ponto Coord Coord Coord
+    deriving (Show)
+
+distancia :: Pontos -> Pontos -> Double
+distancia (Ponto a b c) (Ponto x y z) =
+    sqrt( (a - x) ^ 2 + (b - y) ^ 2  + (c - z) ^ 2 )
+
+-- Compreensão de lista pra pritar uma matriz
+grid :: Int -> Int -> [(Int, Int)]
+grid 0 0 = [(0, 0)]
+
